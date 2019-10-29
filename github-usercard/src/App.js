375c5339a14801ef5207cf5cd93e3cadbcb5d8fb
Mauricio Degregori <degregori2@gmail.com>
2019-10-29 15:52:18 -0500

    app created

---
+++ github-usercard/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Nobody owns the sky!!</h1>
+    </div>
+  );
+}
+
+export default App;
